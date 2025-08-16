7f6f40702edbd4463783070761c5e9285a56559d
EnricoMustafa <enricobrasil123@gmail.com>
2025-08-16 01:43:04 -0300

    service

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -4,7 +4,9 @@ import { MatCardModule} from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { Cliente } from './cliente';
+import { MatIcon } from '@angular/material/icon';
+import { ClienteService } from '../cliente.service';
 @Component({
   selector: 'app-cadastro',
   imports: [
@@ -13,11 +15,20 @@ import { MatInputModule } from '@angular/material/input';
       FormsModule,
       MatFormFieldModule,
       MatInputModule,
+      MatIcon
       
     ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cliente: Cliente =  Cliente.newCliente();
 
+  constructor(private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);
+  }
 }
